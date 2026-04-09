56c8c10e2897cd5e36b9de2bc7a7e0e0a39a1e8d
Assim Fayas <assim@ve.ai>
2026-04-09 21:58:12 +0530

    Fix basePath to /ve-meetings

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/meetings" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/ve-meetings" : "",
 };
 
 export default nextConfig;
